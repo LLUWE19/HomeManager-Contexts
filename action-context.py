6c9dd838211a6c654fb6e686ec5c334a5fb45ec8
Your Name <you@example.com>
2019-09-10 18:13:22 +0100

    added set lights

--- action-context.py
+++ action-context.py
@@ -55,31 +55,37 @@ class HomeManager(object):
         self.start_blocking()
 
     def turn_light_on(self, hermes, intent_message, rooms):
+        """
+        Used to turn on either a specific choice of light/s or turn on every connected light.
+        """
+        print("[DEBUG] (turn_light_on)")
         if len(rooms) > 0:
-            sentence = "Turning on the "
+            sentence = "turning on the "
             for room in rooms:
                 print("Turning on ", room)
                 sentence += " " + room
                 self.steward.light_on(room)
             sentence += " lights"
         else:
-            sentence = "Turning on all the lights"
+            sentence = "lights on"
             self.steward.light_on_all()
         hermes.publish_end_session(intent_message.session_id, sentence)
 
     def turn_light_off(self, hermes, intent_message, rooms):
+        print("[DEBUG] (turn_light_off)")
         if len(rooms) > 0:
-            sentence = "Turning off the "
+            sentence = "turning off the "
             for room in rooms:
                 self.steward.light_off(room)
                 sentence += " " + room
             sentence += " lights"
         else:
             self.steward.light_off_all()
-            sentence = "Turning off all the lights"
+            sentence = "lights off"
         hermes.publish_end_session(intent_message.session_id, sentence)
 
     def set_light_color(self, hermes, intent_message, rooms):
+        print("[DEBUG] (set_light_color)")
         color = self.extract_color(intent_message)
         if len(rooms) > 0:
             sentence = "changing  "
@@ -89,7 +95,7 @@ class HomeManager(object):
             sentence += " lights to " + color
         else:
             self.steward.light_color_all(color)
-            sentence = "changing color for all lights "
+            sentence = "changing lights to " + color
         hermes.publish_end_session(intent_message.session_id, sentence)
 
     def set_light_brightness(self, hermes, intent_message, rooms):
@@ -110,12 +116,14 @@ class HomeManager(object):
         hermes.publish_end_session(intent_message.session_id, sentence)
 
     def start_conversation(self, hermes, intent_message):
+        print("[DEBUG] start_conversation")
         sentence = "welcome home. would you like the lights on"
         self.last_question = sentence
         self.context_commands = False
         hermes.publish_continue_session(intent_message.session_id, sentence, [INTENT_GIVE_ANSWER])
 
     def conversation(self, hermes, intent_message):
+        print("[DEBUG] conversation")
         session_id = intent_message.session_id
 
         answer = None
@@ -162,9 +170,9 @@ class HomeManager(object):
     def master_intent_callback(self,hermes, intent_message):
         rooms = self.extract_house_rooms(intent_message)
         intent_name = intent_message.intent.intent_name
-        print("[DEBUG] intent_name: " + intent_name)
+        print("[DEBUG] (master_intent_callback) intent_name: " + intent_name)
         if self.context_commands:
-            print("In command mode")
+            print("[DEBUG] (master_intent_callback) In command mode")
             if intent_name == INTENT_LIGHT_ON:
                 self.turn_light_on(hermes, intent_message, rooms)
             elif intent_name == INTENT_LIGHT_OFF:
@@ -176,7 +184,7 @@ class HomeManager(object):
             elif intent_name == INTENT_ARRIVE_HOME:
                 self.start_conversation(hermes, intent_message)
         else:
-            print("Conversation mode")
+            print("[DEBUG] (master_intent_callback) Conversation mode")
             self.conversation(hermes, intent_message)
 
     def start_blocking(self):
